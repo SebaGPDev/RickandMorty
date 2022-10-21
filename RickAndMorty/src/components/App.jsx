72bc9d141cad9a3baf55a6016c2307ed107dd264
Sebastian Prieto <35538199+SebaGPDev@users.noreply.github.com>
2022-10-20 23:17:34 -0300

    fetch a la api de RaM, and Hook

--- RickAndMorty/src/components/App.jsx
+++ RickAndMorty/src/components/App.jsx
@@ -1,7 +1,32 @@
+import { useEffect, useState } from "react";
+
 function App() {
+  const [characters, setcharacters] = useState([]);
+  const initialUrl = "https://rickandmortyapi.com/api/character";
+
+  // const getCharacters = async (url) => {
+  //   fetch(url)
+  //     .then((response) => response.json())
+  //     .then((data) => console.log(data))
+  //     .catch((error) => console.error(error));
+  //   };
+  const getCharacters = async (url) => {
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      setcharacters(data.results);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getCharacters(initialUrl);
+  }, []);
+
   return (
-    <div>
-      <h1>Hola</h1>
+    <div className="mt-5 container">
+      <h3>Hola</h3>
     </div>
   );
 }

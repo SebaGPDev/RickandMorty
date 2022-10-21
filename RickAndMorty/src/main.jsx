5d33081dc3fa64bda2b770539b6dcbcc8d1ddfd5
Sebastian Prieto <35538199+SebaGPDev@users.noreply.github.com>
2022-10-21 10:44:48 -0300

    changes

--- RickAndMorty/src/main.jsx
+++ RickAndMorty/src/main.jsx
@@ -1,14 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App";
-import Navbar from "./components/Navbar";
-import Pagination from "./components/Pagination";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Navbar brand="Rick and Morty" />
-    <Pagination />
     <App />
-    <Pagination />
   </React.StrictMode>
 );

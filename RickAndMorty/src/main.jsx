e805427492996c05ba30c1ddc6bec5ef58407dbb
Sebastian Prieto <35538199+SebaGPDev@users.noreply.github.com>
2022-10-20 23:16:06 -0300

    added Navbar component

--- RickAndMorty/src/main.jsx
+++ RickAndMorty/src/main.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App";
+import Navbar from "./components/Navbar";
+
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <Navbar brand="Rick and Morty" />
     <App />
   </React.StrictMode>
 );
